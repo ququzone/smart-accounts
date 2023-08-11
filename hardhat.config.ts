d9db31a4e0ea1813b6acae12cfe0c45969eb17b1
ququzone <xueping.yang@gmail.com>
2023-08-11 12:08:48 +0800

    initial project

---
+++ hardhat.config.ts
@@ -0,0 +1,46 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const importToml = require('import-toml')
+const foundryConfig = importToml.sync('foundry.toml')
+
+const config: HardhatUserConfig = {
+  networks: {
+    hardhat: {
+      allowUnlimitedContractSize: true,
+    },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+  },
+  solidity: {
+    version: foundryConfig.profile.default.solc_version,
+    settings: {
+      viaIR: foundryConfig.profile.default.via_ir,
+      optimizer: {
+        enabled: true,
+        runs: foundryConfig.profile.default.optimizer_runs,
+      },
+      metadata: {
+        // do not include the metadata hash, since this is machine dependent
+        // and we want all generated code to be deterministic
+        // https://docs.soliditylang.org/en/v0.8.20/metadata.html
+        bytecodeHash: 'none',
+      },
+    },
+  },
+};
+
+export default config;
