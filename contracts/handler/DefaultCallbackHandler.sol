bdd30a79cb5d367a90c67b8a18df6ae749eb4352
ququzone <xueping.yang@gmail.com>
2023-08-14 00:07:45 +0800

    add default callback handler

---
+++ contracts/handler/DefaultCallbackHandler.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+import "@account-abstraction/contracts/samples/callback/TokenCallbackHandler.sol";
+
+contract DefaultCallbackHandler is TokenCallbackHandler {}
