4a1bc08f250c2e0ac1b63e3daccda8fec9a06463
ququzone <xueping.yang@gmail.com>
2023-10-10 22:07:07 +0800

    add verifying paymaster deploy

---
+++ deploy/paymaster.ts
@@ -0,0 +1,30 @@
+import { ethers } from 'hardhat'
+import { DeployFunction } from 'hardhat-deploy/types'
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { addresses } from '../src/config'
+
+const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, network } = hre
+  const { deployer } = await getNamedAccounts()
+  const { deploy } = deployments
+
+  let entryPointAddr = ''
+  if (network.name == 'dev') {
+    const entryPoint = await ethers.getContract('EntryPoint')
+    entryPointAddr = entryPoint.address
+  } else {
+    // @ts-ignore
+    entryPointAddr = addresses[network.name].entrypoint
+  }
+
+  await deploy('VerifyingPaymaster', {
+    from: deployer,
+    args: [entryPointAddr, deployer],
+    log: true,
+    deterministicDeployment: false,
+  })
+}
+
+deploy.tags = ['paymaster']
+deploy.dependencies = ['core']
+export default deploy
