fb87391c899303b3105f9db4737d49af56fde5e7
ququzone <xueping.yang@gmail.com>
2023-11-26 08:23:12 +0800

    add snark verifier

--- contracts/validators/OIDCSessionOnlyValidator.sol
+++ contracts/validators/OIDCSessionOnlyValidator.sol
@@ -6,6 +6,13 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "../common/Contants.sol";
 import "./BaseValidator.sol";
 
+interface IVerifier {
+    function verifyProof(uint256[2] memory a, uint256[2][2] memory b, uint256[2] memory c, uint256[2] memory input)
+        external
+        view
+        returns (bool r);
+}
+
 // OIDC ZK based validator that can only to add session validator
 contract OIDCSessionOnlyValidator is BaseValidator {
     string public constant override NAME = "OIDC Validator";
